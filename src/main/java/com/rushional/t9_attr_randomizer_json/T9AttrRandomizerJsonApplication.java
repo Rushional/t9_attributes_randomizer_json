e0edcd9370c145e20c38f0cd1bee08d825912497
Rushional <Rushional2@gmail.com>
2022-10-16 18:01:44 +0400

    first commit

---
+++ src/main/java/com/rushional/t9_attr_randomizer_json/T9AttrRandomizerJsonApplication.java
@@ -0,0 +1,13 @@
+package com.rushional.t9_attr_randomizer_json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T9AttrRandomizerJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T9AttrRandomizerJsonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rushional/t9_attr_randomizer_json/T9AttrRandomizerJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rushional.t9_attr_randomizer_json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T9AttrRandomizerJsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
