6dea34d6182e315f18dadd69f9e78a4e99b3394e
Rushional <Rushional2@gmail.com>
2022-11-30 15:38:12 +0400

    Replaced POST with GET There were no arguments, so using POST was wrong there

--- src/main/java/com/rushional/t9_attr_randomizer_json/controllers/RollController.java
+++ src/main/java/com/rushional/t9_attr_randomizer_json/controllers/RollController.java
@@ -9,21 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RollController {
-    @PostMapping("/roll")
-    public String getStatsRoll() {
-        StatRollsSet rollsSet = new StatRollsSet();
-        //Creating the ObjectMapper object
-        ObjectMapper mapper = new ObjectMapper();
-        //Converting the Object to JSONString
-        try {
-            return mapper.writeValueAsString(rollsSet);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return "everything failed and I didn't bother to make the code better as of yet";
-    }
-
-//    TODO: remove this it was just to test and see the JSON in a browser
     @GetMapping("/roll")
     public String showJson() {
         StatRollsSet rollsSet = new StatRollsSet();
